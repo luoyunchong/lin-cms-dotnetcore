fecc73f68cc25939894baeb22773218a123f1870
IGeekFan <luoyunchong@foxmail.com>
2019-07-01 10:53:56 +0800

    初始始化项目

---
+++ LinCms.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LinCms.Domain
+{
+    public class Class1
+    {
+    }
+}
--- LinCmsCore.Web/Controllers/ValuesController.cs
+++ LinCmsCore.Web/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace LinCmsCore.Controllers
+namespace LinCmsCore.Web.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
