150b065ea89742a4ceed75602cd0f593a92313df
IGeekFan <luoyunchong@foxmail.com>
2019-07-09 15:44:33 +0800

    add domain 实体

---
+++ src/LinCms.Web/Domain/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeSql.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace LinCms.Web.Domain
+{
+    [JsonObject(MemberSerialization.OptIn), Table(Name = "book")]
+    public class Book : FullAduitEntity
+    {
+        [Column(DbType = "varchar(30)")]
+        public string Author { get; set; } = string.Empty;
+
+        [Column(DbType = "varchar(50)")]
+        public string Image { get; set; } = string.Empty;
+
+        [Column(DbType = "varchar(1000)")]
+        public string Summary { get; set; } = string.Empty;
+
+        [Column(DbType = "varchar(50)")]
+        public string Title { get; set; } = string.Empty;
+
+    }
+
+}
--- src/LinCms.Web/Domain/LinAuth.cs
+++ src/LinCms.Web/Domain/LinAuth.cs
@@ -1,9 +1,22 @@
-﻿namespace LinCms.Web.Domain
+﻿using FreeSql.DataAnnotations;
+
+namespace LinCms.Web.Domain
 {
+    [Table(Name = "lin_auth")]
     public class LinAuth : Entity
-    {
+    {   /// <summary>
+        /// 权限字段
+        /// </summary>
         public int GroupId { get; set; }
+        /// <summary>
+        /// 所属权限组id
+        /// </summary>
+        [Column(DbType = "varchar(60)")]
         public string Auth { get; set; }
+        /// <summary>
+        /// 权限的模块
+        /// </summary>
+        [Column(DbType = "varchar(50)")]
         public string Module { get; set; }
     }
 }
