234b1090fef29c9f1825f3a1c10a75454cae6186
luoyunchong <luoyunchong@foxmail.com>
2019-07-20 20:33:20 +0800

    Swagger重写PascalCase，改成SnakeCase模式

---
+++ src/LinCms.Web/Middleware/SnakeCaseQueryParametersApiDescriptionProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinCms.Zero.Extensions;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace LinCms.Web.Middleware
+{
+    public class SnakeCaseQueryParametersApiDescriptionProvider : IApiDescriptionProvider
+    {
+        public int Order => 1;
+
+        public void OnProvidersExecuted(ApiDescriptionProviderContext context)
+        {
+        }
+
+        public void OnProvidersExecuting(ApiDescriptionProviderContext context)
+        {
+            foreach (var parameter in context.Results.SelectMany(x => x.ParameterDescriptions).Where(x => x.Source.Id == "Query" || x.Source.Id == "ModelBinding"))
+            {
+                parameter.Name = parameter.Name.ToSnakeCase();
+            }
+        }
+    }
+}
