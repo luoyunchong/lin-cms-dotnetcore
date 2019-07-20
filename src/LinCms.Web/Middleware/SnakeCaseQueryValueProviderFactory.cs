67b2f1fae92e6d2f7117d64c4577543d158f6787
luoyunchong <luoyunchong@foxmail.com>
2019-07-20 20:33:20 +0800

    Swagger重写PascalCase，改成SnakeCase模式

---
+++ src/LinCms.Web/Middleware/SnakeCaseQueryValueProviderFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using LinCms.Zero.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace LinCms.Web.Middleware
+{
+    public class SnakeCaseQueryValueProvider : QueryStringValueProvider
+    {
+        public SnakeCaseQueryValueProvider(
+            BindingSource bindingSource,
+            IQueryCollection values,
+            CultureInfo culture)
+            : base(bindingSource, values, culture)
+        {
+        }
+
+        public override bool ContainsPrefix(string prefix)
+        {
+            return base.ContainsPrefix(prefix.ToSnakeCase());
+        }
+
+        public override ValueProviderResult GetValue(string key)
+        {
+            return base.GetValue(key.ToSnakeCase());
+        }
+    }
+    public class SnakeCaseQueryValueProviderFactory : IValueProviderFactory
+    {
+        public Task CreateValueProviderAsync(ValueProviderFactoryContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var valueProvider = new SnakeCaseQueryValueProvider(
+                BindingSource.Query,
+                context.ActionContext.HttpContext.Request.Query,
+                CultureInfo.CurrentCulture);
+
+            context.ValueProviders.Add(valueProvider);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/LinCms.Zero/Extensions/StringExtensions.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinCms.Zero.Extensions
+{
+    public static class StringExtensions
+    {
+        private static readonly char[] Delimeters = { ' ', '-', '_' };
+
+        public static string ToPascalCase(this string source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return SymbolsPipe(
+                source,
+                '\0',
+                (s, i) => new char[] { char.ToUpperInvariant(s) });
+        }
+
+        public static string ToCamelCase(this string source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return SymbolsPipe(
+                source,
+                '\0',
+                (s, disableFrontDelimeter) =>
+                {
+                    if (disableFrontDelimeter)
+                    {
+                        return new char[] { char.ToLowerInvariant(s) };
+                    }
+
+                    return new char[] { char.ToUpperInvariant(s) };
+                });
+        }
+
+        public static string ToKebabCase(this string source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return SymbolsPipe(
+                source,
+                '-',
+                (s, disableFrontDelimeter) =>
+                {
+                    if (disableFrontDelimeter)
+                    {
+                        return new char[] { char.ToLowerInvariant(s) };
+                    }
+
+                    return new char[] { '-', char.ToLowerInvariant(s) };
+                });
+        }
+
+        public static string ToSnakeCase(this string source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return SymbolsPipe(
+                source,
+                '_',
+                (s, disableFrontDelimeter) =>
+                {
+                    if (disableFrontDelimeter)
+                    {
+                        return new char[] { char.ToLowerInvariant(s) };
+                    }
+
+                    return new char[] { '_', char.ToLowerInvariant(s) };
+                });
+        }
+
+        public static string ToTrainCase(this string source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return SymbolsPipe(
+                source,
+                '-',
+                (s, disableFrontDelimeter) =>
+                {
+                    if (disableFrontDelimeter)
+                    {
+                        return new char[] { char.ToUpperInvariant(s) };
+                    }
+
+                    return new char[] { '-', char.ToUpperInvariant(s) };
+                });
+        }
+
+        private static string SymbolsPipe(
+            string source,
+            char mainDelimeter,
+            Func<char, bool, char[]> newWordSymbolHandler)
+        {
+            var builder = new StringBuilder();
+
+            bool nextSymbolStartsNewWord = true;
+            bool disableFrontDelimeter = true;
+            for (var i = 0; i < source.Length; i++)
+            {
+                var symbol = source[i];
+                if (Delimeters.Contains(symbol))
+                {
+                    if (symbol == mainDelimeter)
+                    {
+                        builder.Append(symbol);
+                        disableFrontDelimeter = true;
+                    }
+
+                    nextSymbolStartsNewWord = true;
+                }
+                else if (!char.IsLetterOrDigit(symbol))
+                {
+                    builder.Append(symbol);
+                    disableFrontDelimeter = true;
+                    nextSymbolStartsNewWord = true;
+                }
+                else
+                {
+                    if (nextSymbolStartsNewWord || char.IsUpper(symbol))
+                    {
+                        builder.Append(newWordSymbolHandler(symbol, disableFrontDelimeter));
+                        disableFrontDelimeter = false;
+                        nextSymbolStartsNewWord = false;
+                    }
+                    else
+                    {
+                        builder.Append(symbol);
+                    }
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
