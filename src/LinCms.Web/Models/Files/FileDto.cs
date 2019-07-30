92a82ffa9b469fd2e984db6ccca298efd9b03c1a
luoyunchong <luoyunchong@foxmail.com>
2019-07-30 21:19:09 +0800

    上传文件验证md5待完成。

---
+++ src/LinCms.Web/Models/Files/FileDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinCms.Zero.Domain;
+
+namespace LinCms.Web.Models.Files
+{
+    public class FileDto:Entity
+    {
+        public string Key { get; set; }
+        public string Path { get; set; }
+        public string Url { get; set; }
+    }
+}
