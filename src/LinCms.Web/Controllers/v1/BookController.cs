308d8008a85db1bb83a1055a74fb23f89871db26
luoyunchong <luoyunchong@foxmail.com>
2019-08-01 16:57:12 +0800

    update:书管理"软删除效果。

--- src/LinCms.Web/Controllers/v1/BookController.cs
+++ src/LinCms.Web/Controllers/v1/BookController.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using FreeSql;
 using LinCms.Web.Data.Authorization;
 using LinCms.Web.Models.v1.Books;
+using LinCms.Web.Repositories;
 using LinCms.Zero.Data;
 using LinCms.Zero.Domain;
 using LinCms.Zero.Exceptions;
@@ -15,11 +16,11 @@ namespace LinCms.Web.Controllers.v1
     [ApiController]
     public class BookController : ControllerBase
     {
-        private readonly IFreeSql _freeSql;
+        private readonly AuditBaseRepository<Book> _bookRepository;
         private readonly IMapper _mapper;
-        public BookController(IFreeSql freeSql, IMapper mapper)
+        public BookController(AuditBaseRepository<Book> bookRepository, IMapper mapper)
         {
-            _freeSql = freeSql;
+            _bookRepository = bookRepository;
             _mapper = mapper;
         }
 
@@ -27,60 +28,62 @@ namespace LinCms.Web.Controllers.v1
         [LinCmsAuthorize("删除图书", "图书")]
         public ResultDto DeleteBook(int id)
         {
-            _freeSql.Delete<Book>(id).ExecuteAffrows();
+            _bookRepository.Delete(new Book { Id = id });
             return ResultDto.Success();
         }
 
         [HttpGet]
         public List<Book> Get()
         {
-            return _freeSql.Select<Book>().ToList();
+            return _bookRepository.Select.OrderByDescending(r=>r.Id).ToList();
         }
 
         [HttpGet("{id}", Name = "Get")]
         public BookDto Get(int id)
         {
-            Book book = _freeSql.Select<Book>().Where(a => a.Id == id).ToOne();
+            Book book = _bookRepository.Select.Where(a => a.Id == id).ToOne();
             return _mapper.Map<BookDto>(book);
         }
 
         [HttpPost]
         public ResultDto Post([FromBody] CreateUpdateBookDto createBook)
         {
-            bool exist = _freeSql.Select<Book>().Any(r => r.Title == createBook.Title);
+            bool exist = _bookRepository.Select.Any(r => r.Title == createBook.Title);
             if (exist)
             {
                 throw new LinCmsException("图书已存在");
             }
 
             Book book = _mapper.Map<Book>(createBook);
-            _freeSql.Insert(book).ExecuteAffrows();
+            _bookRepository.Insert(book);
             return ResultDto.Success("新建图书成功");
         }
 
         [HttpPut("{id}")]
         public ResultDto Put(int id, [FromBody] CreateUpdateBookDto updateBook)
         {
-            //这种找不到逻辑，我一般不写的。
-            bool bookExist = _freeSql.Select<Book>().Any(r => r.Id == id);
-            if (!bookExist)
+            Book book = _bookRepository.Select.Where(r => r.Id == id).ToOne();
+            if (book==null)
             {
                 throw new LinCmsException("没有找到相关书籍");
             }
 
-            bool exist = _freeSql.Select<Book>().Any(r => r.Title == updateBook.Title && r.Id != id);
+            bool exist = _bookRepository.Select.Any(r => r.Title == updateBook.Title && r.Id != id);
             if (exist)
             {
                 throw new LinCmsException("图书已存在");
             }
 
-            _freeSql.Update<Book>(id).Set(a => new Book()
-            {
-                Title = updateBook.Title,
-                Author = updateBook.Author,
-                Image = updateBook.Image,
-                Summary = updateBook.Summary,
-            }).ExecuteAffrows();
+            //book.Image = updateBook.Image;
+            //book.Title = updateBook.Title;
+            //book.Author = updateBook.Author;
+            //book.Summary = updateBook.Summary;
+            //book.Summary = updateBook.Summary;
+
+            //使用AutoMapper方法简化类与类之间的转换过程
+            _mapper.Map(updateBook,book);
+
+            _bookRepository.Update(book);
 
             return ResultDto.Success("更新图书成功");
         }
