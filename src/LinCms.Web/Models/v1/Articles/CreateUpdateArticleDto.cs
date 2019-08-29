504b705d2961cfa58d91310b932d3c948cda35ea
luoyunchong <luoyunchong@foxmail.com>
2019-08-29 14:52:16 +0800

    #1 add article and reply 实现博客基础

---
+++ src/LinCms.Web/Models/v1/Articles/CreateUpdateArticleDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinCms.Web.Models.v1.Articles
+{
+    public class CreateUpdateArticleDto
+    {
+        public int Id { get; set; }
+        public int? FId { get; set; }
+        [StringLength(200)]
+        public string Title { get; set; }
+        [StringLength(400)]
+        public string Keywords { get; set; }
+        [StringLength(400)]
+        public string Source { get; set; }
+        [StringLength(400)]
+        public string Excerpt { get; set; }
+        public string Content { get; set; }
+        public int ViewHits { get; set; }
+        public int CommentQuantity { get; set; }
+        public int PointQuantity { get; set; }
+        [StringLength(400)]
+        public string Thumbnail { get; set; }
+        public bool IsAudit { get; set; }
+        public bool Recommend { get; set; }
+        public bool IsStickie { get; set; }
+        [StringLength(50)]
+        public string Archive { get; set; }
+
+        [StringLength(50)]
+        public string ArticleType { get; set; }
+
+        public int Editor { get; set; } = 1;
+    }
+
+}
---
+++ src/LinCms.Zero/Domain/Blog/Reply.cs
@@ -0,0 +1,69 @@
+﻿using FreeSql.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinCms.Zero.Domain.Blog
+{
+    [Table(Name = "blog_reply")]
+    public class Reply : FullAduitEntity
+    {
+        public int? PId { get; set; }
+        /// <summary>
+        /// @的用户名，用于前台的显示效果
+        /// </summary>
+        [Column(DbType = "varchar(50)")]
+        public string PName { get; set; }
+        public string Text { get; set; }
+        [Column(DbType = "varchar(50)")]
+        public string Ip { get; set; }
+        [Column(DbType = "varchar(50)")]
+        public string Agent { get; set; }
+        //系统
+        [Column(DbType = "varchar(50)")]
+        public string System { get; set; }
+        /// <summary>
+        /// 地理位置
+        /// </summary>
+        [Column(DbType = "varchar(50)")]
+        public string GeoPosition { get; set; }
+
+        [Column(DbType = "varchar(50)")]
+        public string Qq { get; set; }
+        [Column(DbType = "varchar(50)")]
+        public string AuName { get; set; }
+        [Column(DbType = "varchar(400)")]
+        public string PersonalWebsite { get; set; }
+        [Column(DbType = "varchar(50)")]
+        public string AuEmail { get; set; }
+        [Column(DbType = "varchar(50)")]
+        public string UserHost { get; set; }
+        /// <summary>
+        /// 是否已审核
+        /// </summary>
+        public bool? IsAduit { get; set; }
+        /// <summary>
+        /// 评论人的头像，如果未登录状态下，则随机生成一个头像地址。已登录状态下，取用户表的头像地址
+        /// </summary>
+        [Column(DbType = "varchar(500)")]
+        public string Avatar { get; set; }
+    }
+
+    [Table(Name = "blog_review")]
+    public class Review : Reply
+    {
+        /// <summary>
+        /// 关于文章id
+        /// </summary>
+        public int? AId { get; set; }
+    }
+
+    /// <summary>
+    /// 留言板
+    /// </summary>
+    [Table(Name = "blog_guestBook")]
+    public class GuestBook : Reply
+    {
+
+    }
+}
