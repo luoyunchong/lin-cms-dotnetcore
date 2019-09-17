f759c025bd15caaeb65d809586b56a62698399ae
IGeekFan <710277267@qq.com>
2019-09-17 18:12:29 +0800

    #1 作者为空时，当前登录昵称

--- src/LinCms.Web/Models/v1/Articles/ArticleDto.cs
+++ src/LinCms.Web/Models/v1/Articles/ArticleDto.cs
@@ -1,30 +1,26 @@
 ﻿using LinCms.Zero.Common;
 using LinCms.Zero.Domain;
+using LinCms.Zero.Domain.Blog;
 using System;
 namespace LinCms.Web.Models.v1.Articles
 {
     public class ArticleDto : Entity, ICreateAduitEntity
     {
-
         //类别Id
-        public int? FId { get; set; }
-        //类别编码  
-        public string TypeCode { get; set; }
+        public int? ClassifyId { get; set; }
         //类别名称 
-        public string TypeName { get; set; }
-        //发布人
-        public string NickName { get; set; }
+        public string ClassifyName { get; set; }
         //几小时/秒前
         public string TimeSpan => LinCmsUtils.GetTimeDifferNow(this.CreateTime.ToDateTime());
 
         private readonly DateTime _now = DateTime.Now;
         public bool IsNew => DateTime.Compare(_now.AddDays(-2), this.CreateTime.ToDateTime()) <= 0;
 
+        public string Content { get; set; }
         public string Title { get; set; }
         public string Keywords { get; set; }
         public string Source { get; set; }
         public string Excerpt { get; set; }
-        public string Content { get; set; }
         public int ViewHits { get; set; }
         public int CommentQuantity { get; set; }
         public int PointQuantity { get; set; }
@@ -34,9 +30,10 @@ namespace LinCms.Web.Models.v1.Articles
         public bool Recommend { get; set; }
         public bool IsStickie { get; set; }
         public string Archive { get; set; }
-        public string ArticleType { get; set; }
+        public ArticleType ArticleType { get; set; }
         public int Editor { get; set; } = 1;
         public long? CreateUserId { get; set; }
         public DateTime CreateTime { get; set; }
+        public string Author { get; set; }
     }
 }
