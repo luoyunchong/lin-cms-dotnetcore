74524136e3fdd1c2454fab19c03c4c605a9ce2c5
IGeekFan <luoyunchong@foxmail.com>
2019-10-06 21:16:58 +0800

    Add:用户评论随笔，审核，列表接口

---
+++ src/LinCms.Web/AutoMapper/v1/CommentProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LinCms.Web.Models.v1.Comments;
+using LinCms.Zero.Domain.Blog;
+
+namespace LinCms.Web.AutoMapper.v1
+{
+    public class CommentProfile : Profile
+    {
+        public CommentProfile()
+        {
+            CreateMap<CreateCommentDto, Comment>();
+            CreateMap<Comment, CommentDto>();
+        }
+    }
+}
--- src/LinCms.Zero/Common/LinCmsUtils.cs
+++ src/LinCms.Zero/Common/LinCmsUtils.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 using Microsoft.AspNetCore.Http;
+using MySqlX.XDevAPI.Common;
+using Newtonsoft.Json;
 
 namespace LinCms.Zero.Common
 {
@@ -115,5 +118,113 @@ namespace LinCms.Zero.Common
 
             return "1秒前";
         }
+
+        /// <summary>
+        /// 获取操作系统的名字
+        /// </summary>
+        /// <param name="userAgent"></param>
+        /// <returns></returns>
+        public static string GetOsNameByUserAgent(string userAgent)
+        {
+            string osVersion = "未知";
+
+            if (userAgent.Contains("NT 10.0"))
+            {
+                osVersion = "Windows 10";
+            }
+            else if (userAgent.Contains("NT 6.3"))
+            {
+                osVersion = "Windows 8.1";
+            }
+            else if (userAgent.Contains("NT 6.2"))
+            {
+                osVersion = "Windows 8";
+            }
+            else if (userAgent.Contains("NT 6.1"))
+            {
+                osVersion = "Windows 7";
+            }
+            else if (userAgent.Contains("NT 6.1"))
+            {
+                osVersion = "Windows 7";
+            }
+            else if (userAgent.Contains("NT 6.0"))
+            {
+                osVersion = "Windows Vista/Server 2008";
+            }
+            else if (userAgent.Contains("NT 5.2"))
+            {
+                osVersion = userAgent.Contains("64") ? "Windows XP" : "Windows Server 2003";
+            }
+            else if (userAgent.Contains("NT 5.1"))
+            {
+                osVersion = "Windows XP";
+            }
+            else if (userAgent.Contains("NT 5"))
+            {
+                osVersion = "Windows 2000";
+            }
+            else if (userAgent.Contains("NT 4"))
+            {
+                osVersion = "Windows NT4";
+            }
+            else if (userAgent.Contains("Me"))
+            {
+                osVersion = "Windows Me";
+            }
+            else if (userAgent.Contains("98"))
+            {
+                osVersion = "Windows 98";
+            }
+            else if (userAgent.Contains("95"))
+            {
+                osVersion = "Windows 95";
+            }
+            else if (userAgent.Contains("Mac"))
+            {
+                osVersion = "Mac";
+            }
+            else if (userAgent.Contains("Unix"))
+            {
+                osVersion = "UNIX";
+            }
+            else if (userAgent.Contains("Linux"))
+            {
+                osVersion = "Linux";
+            }
+            else if (userAgent.Contains("SunOS"))
+            {
+                osVersion = "SunOS";
+            }
+            return osVersion;
+        }
+
+
+        /// <summary>
+        /// https://cloud.tencent.com/developer/article/1152362
+        /// 获取本机IP、地区及运营商 
+        /// </summary>
+        /// <param name="ipAddres"></param>
+        /// <returns></returns>
+        public static IpQueryResult IpQueryCity(string ipAddres)
+        {
+            Uri url = new Uri("http://ip.360.cn/IPQuery/ipquery?ip=" + ipAddres);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Timeout = 1000 * 5;
+            request.KeepAlive = false;
+            request.Method = "GET";
+            WebResponse resoponse = request.GetResponse();
+            string pageHtml;
+            // ReSharper disable once AssignNullToNotNullAttribute
+            using (var stream = new StreamReader(resoponse.GetResponseStream()))
+            {
+                pageHtml = stream.ReadToEnd();
+            }
+            IpQueryResult result = JsonConvert.DeserializeObject<IpQueryResult>(pageHtml);
+
+            return result;
+        }
+
+
     }
 }
