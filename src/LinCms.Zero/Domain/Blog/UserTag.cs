7938f47bf4d149b04214cc532c7f27e2911fb5c7
IGeekFan <luoyunchong@foxmail.com>
2019-11-19 23:57:23 +0800

    add user tag

---
+++ src/LinCms.Zero/Domain/Blog/UserTag.cs
@@ -0,0 +1,15 @@
+﻿using FreeSql.DataAnnotations;
+using System;
+using System.Collections.Generic;
+
+namespace LinCms.Zero.Domain.Blog
+{
+    [Table(Name = "blog_user_tag")]
+    public class UserTag : Entity<Guid>
+    {
+        public Guid TagId { get; set; }
+        public Guid UserId { get; set; }
+        public virtual ICollection<Tag> Tags { get; set; }
+        public virtual ICollection<LinUser> LinUser { get; set; }
+    }
+}
