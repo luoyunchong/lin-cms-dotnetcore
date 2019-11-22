5c3165df95a78a17c8be41a74261ea9c64c7b61a
igeekfan <igeekfan@foxmail.com>
2019-11-22 16:43:05 +0800

    update collections and freesql

--- src/LinCms.Zero/Extensions/CollectionsExtensions.cs
+++ src/LinCms.Zero/Extensions/CollectionsExtensions.cs
@@ -7,33 +7,34 @@ namespace LinCms.Zero.Extensions
 {
     public static class CollectionsExtensions
     {
+        /// <summary>
+        /// Page 方法是从1开始
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="pageDto"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
         public static ISelect<T1, T2> ToPager<T1, T2>(this ISelect<T1, T2> source, PageDto pageDto, out long count)
             where T1 : class
             where T2 : class
         {
-            count = source.Count();
-            //Page 方法是从1开始
-            return source.Page(pageDto.Page + 1, pageDto.Count);
+            return source.Count(out count).Page(pageDto.Page + 1, pageDto.Count);
         }
 
         public static ISelect<TEntity> ToPager<TEntity>(this ISelect<TEntity> source, PageDto pageDto, out long count) where TEntity : class
         {
-            count = source.Count();
-            //Page 方法是从1开始
-            return source.Page(pageDto.Page + 1, pageDto.Count);
+            return source.Count(out count).Page(pageDto.Page + 1, pageDto.Count);
         }
         public static List<TEntity> ToPagerList<TEntity>(this ISelect<TEntity> source, PageDto pageDto, out long count) where TEntity : class
         {
-            count = source.Count();
-            //Page 方法是从1开始
-            return source.Page(pageDto.Page + 1, pageDto.Count).ToList();
+            return source.Count(out count).Page(pageDto.Page + 1, pageDto.Count).ToList();
         }
 
         public static List<TResult> ToPagerList<TEntity, TResult>(this ISelect<TEntity> source, PageDto pageDto, out long count) where TEntity : class
         {
-            count = source.Count();
-
-            return source.Page(pageDto.Page + 1, pageDto.Count).ToList<TResult>();
+            return source.Count(out count).Page(pageDto.Page + 1, pageDto.Count).ToList<TResult>();
         }
     }
 }
