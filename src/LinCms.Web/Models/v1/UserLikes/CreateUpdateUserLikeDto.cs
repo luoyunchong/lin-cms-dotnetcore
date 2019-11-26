ab73eddd8ba308eeff732da790fabcce87090a2b
IGeekFan <luoyunchong@foxmail.com>
2019-11-27 01:43:16 +0800

    Update:分类专栏文章数量 ，标签文章数量

--- src/LinCms.Web/Models/v1/UserLikes/CreateUpdateUserLikeDto.cs
+++ src/LinCms.Web/Models/v1/UserLikes/CreateUpdateUserLikeDto.cs
@@ -8,7 +8,9 @@ namespace LinCms.Web.Models.v1.UserLikes
     public class CreateUpdateUserLikeDto
     {
         public Guid SubjectId { get; set; }
-
+        /// <summary>
+        /// 1.文章 2 评论
+        /// </summary>
         public int SubjectType { get; set; }
     }
 }
