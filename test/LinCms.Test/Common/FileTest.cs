fa17cbdfc528462e81981635d652f8e645086a71
igeekfan <igeekfan@foxmail.com>
2019-12-03 16:51:46 +0800

    add：敏感词处理

---
+++ test/LinCms.Test/Common/FileTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace LinCms.Test.Common
+{
+    public class FileTest
+    {
+        [Fact]
+        public void test()
+        {
+            string url = "https://github.com/fighting41love/funNLP/raw/master/data/%E6%95%8F%E6%84%9F%E8%AF%8D%E5%BA%93/%E5%B9%BF%E5%91%8A.txt";
+            string lastWriteTime = new FileInfo(url).LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss");
+            ;
+        }
+    }
+}
