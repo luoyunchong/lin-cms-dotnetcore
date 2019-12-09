b939c22593a251ea1d5dcbef0ea990e88e20dd41
IGeekFan <luoyunchong@foxmail.com>
2019-12-09 23:49:14 +0800

    add 用户关注另一个用户

---
+++ src/LinCms.Zero/Domain/Blog/UserFollow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreeSql.DataAnnotations;
+
+namespace LinCms.Zero.Domain.Blog
+{
+    /// <summary>
+    /// 用户关注用户
+    /// </summary>
+    [Table(Name = "blog_user_follow")]
+    public class UserFollow : Entity<Guid>, ICreateAduitEntity
+    {
+        /// <summary>
+        /// 被关注的用户Id
+        /// </summary>
+        public long FollowUserId { get; set; }
+        /// <summary>
+        /// 关注的用户Id
+        /// </summary>
+        public long? CreateUserId { get; set; }
+        public DateTime CreateTime { get; set; }
+
+        [Navigate("CreateUserId")]
+        public virtual LinUser LinUser { get; set; }
+        [Navigate("FollowUserId")]
+        public virtual LinUser FollowUser { get; set; }
+    }
+}
