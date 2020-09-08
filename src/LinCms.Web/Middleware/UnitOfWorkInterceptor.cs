40f0c935bbd5be6d0a952cd1a751ed3aebef8f6f
luoyunchong <luoyunchong@foxmail.com>
2020-09-09 03:07:39 +0800

    fix aop unitofwork dispose async autofac problem

--- src/LinCms.Web/Middleware/UnitOfWorkInterceptor.cs
+++ src/LinCms.Web/Middleware/UnitOfWorkInterceptor.cs
@@ -10,6 +10,7 @@ namespace LinCms.Middleware
     public class UnitOfWorkInterceptor : IInterceptor
     {
         private readonly UnitOfWorkAsyncInterceptor asyncInterceptor;
+
         public UnitOfWorkInterceptor(UnitOfWorkAsyncInterceptor interceptor)
         {
             asyncInterceptor = interceptor;
@@ -56,12 +57,12 @@ namespace LinCms.Middleware
         {
             if (TryBegin(invocation))
             {
-                int? hashCode = _unitOfWork?.GetHashCode();
+                int? hashCode = _unitOfWork.GetHashCode();
                 try
                 {
                     invocation.Proceed();
                     _logger.LogInformation($"----- 拦截同步执行的方法-事务 {hashCode} 提交前----- ");
-                    _unitOfWork?.Commit();
+                    _unitOfWork.Commit();
                     _logger.LogInformation($"----- 拦截同步执行的方法-事务 {hashCode} 提交成功----- ");
                 }
                 catch
@@ -70,7 +71,10 @@ namespace LinCms.Middleware
                     _unitOfWork.Rollback();
                     throw;
                 }
-
+                finally
+                {
+                    _unitOfWork.Dispose();
+                }
             }
             else
             {
@@ -82,46 +86,41 @@ namespace LinCms.Middleware
         /// 拦截返回结果为Task的方法
         /// </summary>
         /// <param name="invocation"></param>
-        public async void InterceptAsynchronous(IInvocation invocation)
+        public void InterceptAsynchronous(IInvocation invocation)
         {
+            invocation.ReturnValue = InternalInterceptAsynchronous(invocation);
+        }
 
+        private async Task InternalInterceptAsynchronous(IInvocation invocation)
+        {
             if (TryBegin(invocation))
             {
-                var methodName = $"{invocation.MethodInvocationTarget.DeclaringType.FullName}.{invocation.Method.Name}()";
-                int? hashCode = _unitOfWork?.GetHashCode();
+                string methodName =
+                    $"{invocation.MethodInvocationTarget.DeclaringType?.FullName}.{invocation.Method.Name}()";
+                int? hashCode = _unitOfWork.GetHashCode();
 
                 using (_logger.BeginScope("_unitOfWork:{hashCode}", hashCode))
                 {
-                    _logger.LogInformation($"----- async Task 开始事务{ hashCode} {methodName}----- ");
+                    _logger.LogInformation($"----- async Task 开始事务{hashCode} {methodName}----- ");
 
                     invocation.Proceed();
 
-                    //try
-                    //{
-                    //    await (Task)invocation.ReturnValue;
-                    //    _unitOfWork?.Commit();
-                    //    _logger.LogInformation($"----- async Task 事务 { hashCode} Commit----- ");
-                    //}
-                    //catch (System.Exception)
-                    //{
-                    //    _unitOfWork?.Rollback();
-                    //    _logger.LogError($"----- async Task 事务 { hashCode} Rollback----- ");
-                    //}
-
-                    await ((Task)invocation.ReturnValue).ContinueWith(
-                     antecedent =>
-                     {
-                         if (antecedent.Exception == null)
-                         {
-                             _unitOfWork?.Commit();
-                             _logger.LogInformation($"----- async Task 事务 { hashCode} Commit----- ");
-                         }
-                         else
-                         {
-                             _unitOfWork?.Rollback();
-                             _logger.LogError($"----- async Task 事务 { hashCode} Rollback----- ");
-                         }
-                     });
+                    try
+                    {
+                        await (Task)invocation.ReturnValue;
+                        _unitOfWork.Commit();
+                        _logger.LogInformation($"----- async Task 事务 {hashCode} Commit----- ");
+                    }
+                    catch (System.Exception)
+                    {
+                        _unitOfWork.Rollback();
+                        _logger.LogError($"----- async Task 事务 {hashCode} Rollback----- ");
+                        throw;
+                    }
+                    finally
+                    {
+                        _unitOfWork.Dispose();
+                    }
                 }
             }
             else
@@ -130,55 +129,50 @@ namespace LinCms.Middleware
             }
         }
 
+
         /// <summary>
         /// 拦截返回结果为Task<TResult>的方法
         /// </summary>
         /// <param name="invocation"></param>
         /// <typeparam name="TResult"></typeparam>
-        public async void InterceptAsynchronous<TResult>(IInvocation invocation)
+        public void InterceptAsynchronous<TResult>(IInvocation invocation)
+        {
+            invocation.ReturnValue = InternalInterceptAsynchronous<TResult>(invocation);
+        }
+        private async Task<TResult> InternalInterceptAsynchronous<TResult>(IInvocation invocation)
         {
             if (TryBegin(invocation))
             {
-                var methodName = $"{invocation.MethodInvocationTarget.DeclaringType.FullName}.{invocation.Method.Name}()";
-                int? hashCode = _unitOfWork?.GetHashCode();
+                string methodName =$"{invocation.MethodInvocationTarget.DeclaringType?.FullName}.{invocation.Method.Name}()";
+                int hashCode = _unitOfWork.GetHashCode();
+                _logger.LogInformation($"----- async Task<TResult> 开始事务{hashCode} {methodName}----- ");
 
-                _logger.LogInformation($"----- async Task<TResult> 开始事务{ hashCode} {methodName}----- ");
-
-                invocation.Proceed();
-
-                //try
-                //{
-                //    TResult result = await (Task<TResult>)invocation.ReturnValue;
-                //    _unitOfWork?.Commit();
-                //    _logger.LogInformation($"----- async Task<TResult> Commit事务{ hashCode}----- ");
-                //}
-                //catch (System.Exception)
-                //{
-                //    _unitOfWork?.Rollback();
-                //    _logger.LogError($"----- async Task<TResult> Rollback事务{ hashCode}----- ");
-                //}
-
-                await ((Task<TResult>)invocation.ReturnValue).ContinueWith(
-                       antecedent =>
-                       {
-                           if (antecedent.Exception == null)
-                           {
-                               _unitOfWork?.Commit();
-                               _logger.LogInformation($"----- async Task<TResult> Commit事务{ hashCode}----- ");
-                           }
-                           else
-                           {
-                               _unitOfWork?.Rollback();
-                               _logger.LogError($"----- async Task<TResult> Rollback事务{ hashCode}----- ");
-                           }
-                       });
+                try
+                {
+                    invocation.Proceed();
+                    TResult result = await (Task<TResult>)invocation.ReturnValue;
+                    _unitOfWork.Commit();
+                    _logger.LogInformation($"----- async Task<TResult> Commit事务{hashCode}----- ");
+                    return result;
+                }
+                catch (System.Exception)
+                {
+                    _unitOfWork.Rollback();
+                    _logger.LogError($"----- async Task<TResult> Rollback事务{hashCode}----- ");
+                    throw;
+                }
+                finally
+                {
+                    _unitOfWork.Dispose();
+                }
             }
             else
             {
                 invocation.Proceed();
+                TResult result = await (Task<TResult>)invocation.ReturnValue;
+                return result;
             }
-        }
 
+        }
     }
 }
-
