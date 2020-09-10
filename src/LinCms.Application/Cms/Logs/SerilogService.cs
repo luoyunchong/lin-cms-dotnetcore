4ce706911dee2555da966832f1331036b4c1a3ab
luoyunchong <luoyunchong@foxmail.com>
2020-09-11 03:05:40 +0800

    fix async InterceptAsynchronous异步问题 add 日志统计

--- src/LinCms.Application/Cms/Logs/SerilogService.cs
+++ src/LinCms.Application/Cms/Logs/SerilogService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FreeSql;
 using LinCms.Data;
@@ -22,10 +23,23 @@ namespace LinCms.Cms.Logs
                 .WhereIf(searchDto.Start.HasValue, r => r.Timestamp >= searchDto.Start.Value)
                 .WhereIf(searchDto.End.HasValue, r => r.Timestamp <= searchDto.End.Value)
                 .WhereIf(!string.IsNullOrEmpty(searchDto.Keyword), r => r.Message.Contains(searchDto.Keyword))
-                .WhereIf(searchDto.LogLevel!=null, r => r.Level==searchDto.LogLevel)
+                .WhereIf(searchDto.LogLevel != null, r => r.Level == searchDto.LogLevel)
                 .OrderByDescending(r => r.Id)
                 .ToPagerListAsync(searchDto, out long count);
             return new PagedResultDto<SerilogDO>(serilogLists, count);
+        }
+
+        public async Task<LogDashboard> GetLogDashboard()
+        {
+            DateTime now = DateTime.Now;
+
+            return new LogDashboard()
+            {
+                AllCount = await _serilogBaseRepository.Select.CountAsync(),
+                TodayCount = await _serilogBaseRepository.Select.Where(x => x.Timestamp >= now.Date && x.Timestamp <= now.Date.AddHours(23).AddMinutes(59).AddSeconds(59)).CountAsync(),
+                HourCount = await _serilogBaseRepository.Select.Where(x => x.Timestamp >= now.AddHours(-1) && x.Timestamp <= now).CountAsync(),
+                UniqueCount = await _serilogBaseRepository.Select.WithSql(@"select count(*) AS TOTAL from app_serilog group by message").CountAsync(),
+            };
         }
     }
 }
