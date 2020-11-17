76795455e294ccf56a7390606edc69ce77285a41
luoyunchong <luoyunchong@foxmail.com>
2020-11-18 01:17:37 +0800

    add cap demo

--- src/LinCms.Application/CapUnitOfWorkExtensions.cs
+++ src/LinCms.Application/CapUnitOfWorkExtensions.cs
@@ -6,16 +6,17 @@ namespace LinCms
 {
     public static class CapUnitOfWorkExtensions
     {
+
+        public static void Flush(this ICapTransaction capTransaction)
+        {
+            capTransaction?.GetType().GetMethod("Flush", BindingFlags.Instance | BindingFlags.NonPublic)?.Invoke(capTransaction, null);
+        }
+
+       
         public static ICapTransaction BeginTransaction(this IUnitOfWork unitOfWork, ICapPublisher publisher, bool autoCommit = false)
         {
             publisher.Transaction.Value = (ICapTransaction)publisher.ServiceProvider.GetService(typeof(ICapTransaction));
             return publisher.Transaction.Value.Begin(unitOfWork.GetOrBeginTransaction(), autoCommit);
-        }
-
-        public static void Flush(this ICapTransaction capTransaction)
-        {
-            capTransaction?.GetType().GetMethod("Flush", BindingFlags.Instance | BindingFlags.NonPublic)
-                ?.Invoke(capTransaction, null);
         }
 
         public static void Commit(this ICapTransaction capTransaction, IUnitOfWork unitOfWork)
@@ -24,11 +25,24 @@ namespace LinCms
             capTransaction.Flush();
         }
 
-        public static void Commit(this IUnitOfWork unitOfWork, ICapTransaction capTransaction)
-        {
-            unitOfWork.Commit();
-            capTransaction.Flush();
-        }
-    }
 
+        //public static ICapTransaction BeginTransaction(this UnitOfWorkManager unitOfWorkManager, ICapPublisher publisher, bool autoCommit = false)
+        //{
+        //    publisher.Transaction.Value = (ICapTransaction)publisher.ServiceProvider.GetService(typeof(ICapTransaction));
+
+        //    IUnitOfWork unitOfWork = unitOfWorkManager.Begin(Propagation.Required);
+
+        //    return publisher.Transaction.Value.Begin(unitOfWork.GetOrBeginTransaction(), autoCommit);
+        //}
+
+        //public static void Commit(this ICapTransaction capTransaction, UnitOfWorkManager unitOfWorkManager)
+        //{
+        //    IUnitOfWork unitOfWork = unitOfWorkManager.Current;
+        //    if (unitOfWork != null)
+        //    {
+        //        unitOfWork.Commit();
+        //    }
+        //    capTransaction.Flush();
+        //}
+    }
 }
