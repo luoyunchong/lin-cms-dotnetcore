a3131e40e36b1d7fcd5d26cef7c531f83eae9f03
luoyunchong <luoyunchong@foxmail.com>
2021-05-11 19:45:34 +0800

    引入 serilog 方便 查看日志。

--- src/LinCms.Scaffolding/CodeScaffolding.cs
+++ src/LinCms.Scaffolding/CodeScaffolding.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
 using Scriban;
 using Scriban.Runtime;
-using System;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,21 +10,21 @@ namespace LinCms.Scaffolding
 {
     public class CodeScaffolding
     {
+        private readonly ILogger<App> _logger;
+        public string TemplatePath { get; }
+        public string OutputDir { get; }
+        public string PostFix { get; set; } = ".txt";
         /// <summary>
         /// 
         /// </summary>
         /// <param name="templatePath">模板的根目录（绝对目录）</param>
         /// <param name="outputDir">输出目录（绝对目录/相对目录）</param>
-        public CodeScaffolding(string templatePath, string outputDir)
+        public CodeScaffolding(string templatePath, string outputDir, ILogger<App> logger)
         {
             TemplatePath = templatePath;
             OutputDir = outputDir;
+            _logger = logger;
         }
-
-        public string TemplatePath { get; }
-        public string OutputDir { get; }
-
-        public string PostFix { get; set; } = ".txt";
 
         public async Task GenerateAsync(object model)
         {
@@ -38,7 +38,7 @@ namespace LinCms.Scaffolding
             foreach (var (path, file) in fileProvider.GetFilesRecursively("./"))
             {
                 string templateText = await file.ReadAsStringAsync(Encoding.UTF8);
-                Console.WriteLine(templateText);
+                _logger.LogInformation(templateText);
 
                 TemplateContext context = new TemplateContext();
                 ScriptObject scriptObject = new ScriptObject();
@@ -48,7 +48,7 @@ namespace LinCms.Scaffolding
 
                 Template template = Template.Parse(templateText);
                 string result = template.Render(context);
-                Console.WriteLine(result);
+                _logger.LogInformation(result);
 
                 Template pathTemplate = Template.Parse(Path.Combine(OutputDir, path.RemovePostFix(PostFix)));
                 string targetFilePathName = pathTemplate.Render(context);
@@ -58,7 +58,7 @@ namespace LinCms.Scaffolding
                 File.WriteAllText(targetFilePathName, result);
                 count += 1;
             }
-            Console.WriteLine(count);
+            _logger.LogInformation($"共生成{count}个");
         }
     }
 }
