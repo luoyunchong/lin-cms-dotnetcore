c22117b32601bf30330ed611c1434888460939e5
igeekfan <luoyunchong@foxmail.com>
2023-06-12 22:21:18 +0800

    update 增加删除判断

--- src/LinCms.Application.Contracts/Blog/Collections/CollectionSearchDto.cs
+++ src/LinCms.Application.Contracts/Blog/Collections/CollectionSearchDto.cs
@@ -1,8 +1,11 @@
 using LinCms.Data;
+using System;
 
 namespace LinCms.Blog.Collections;
 
 public class CollectionSearchDto : PageDto
 {
     public string Name { get; set; }
+
+    public long? UserId { get; set; }
 }
--- src/LinCms.Application/Blog/Collections/CollectionService.cs
+++ src/LinCms.Application/Blog/Collections/CollectionService.cs
@@ -27,13 +27,14 @@ public class CollectionService : CrudAppService<Collection, CollectionDto, Colle
     {
         _collectionRepsitory = collectionRepsitory;
         _artCollectionRepsitory = artCollectionRepsitory;
-    } 
+    }
     #endregion
 
     protected override ISelect<Collection> CreateFilteredQuery(CollectionSearchDto input)
     {
         return base.CreateFilteredQuery(input)
-             .Where(r => r.CreateUserId == CurrentUser.FindUserId())
+             .WhereIf(input.UserId == null || input.UserId == CurrentUser.FindUserId(), r => r.CreateUserId == CurrentUser.FindUserId())
+             .WhereIf(input.UserId != null, r => r.CreateUserId == input.UserId && r.PrivacyType == PrivacyType.Public)
             .WhereIf(input.Name.IsNotNullOrEmpty(), r => r.Name.Contains(input.Name));
     }
 
@@ -41,6 +42,11 @@ public class CollectionService : CrudAppService<Collection, CollectionDto, Colle
     [Transactional]
     public override async Task DeleteAsync(Guid collectionId)
     {
+        Collection collection = await _collectionRepsitory.Where(r => r.Id == collectionId).FirstAsync();
+        if (collection.CreateUserId != CurrentUser.FindUserId())
+        {
+            throw new LinCmsException("您只可删除自己创建的收藏集");
+        }
         await _artCollectionRepsitory.DeleteAsync(r => r.CollectionId == collectionId);
         await _collectionRepsitory.DeleteAsync(r => r.Id == collectionId);
     }
